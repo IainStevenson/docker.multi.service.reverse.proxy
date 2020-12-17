1dece7b78634ea28941d68816ff1ad7e5c95994a
Iain <iainstevenson@live.co.uk>
2020-12-17 06:25:08 +0000

    Routing through to store by default

--- src/WebApp1/Startup.cs
+++ src/WebApp1/Startup.cs
@@ -39,13 +39,13 @@ namespace WebApp1
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
