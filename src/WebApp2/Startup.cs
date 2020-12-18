107f9068c55488987c05699bd16efad07ceef425
Iain <iainstevenson@live.co.uk>
2020-12-18 08:17:51 +0000

    Reverted last change

--- src/WebApp2/Startup.cs
+++ src/WebApp2/Startup.cs
@@ -38,17 +38,17 @@ namespace WebApp2
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseHttpsRedirection();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseStaticFiles();
 
