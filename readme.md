930cde39b23bffb51e8ec04773e5e4bb56652280
Iain <iainstevenson@live.co.uk>
2021-01-06 11:01:00 +0000

    Added Api, Identity and API test client Confirmed wrking version for secure API access via api test client

--- readme.md
+++ readme.md
@@ -662,3 +662,28 @@ After which you can perform actions requiring root access by prefixing other com
 ```
 apt install nano
 ```
+
+
+# Identity Server. API and API Test Client
+
+
+To get these working set identity and API to use the self hosting startup option, then set multipe project startup of Identity, Api and API test client in that order of execution.
+
+
+Following the steps in [the Identity Server 4 Documentation](https://identityserver4.readthedocs.io/en/latest/quickstarts/0_overview.html)
+
+Notes: Project name changes and url path modifications made.
+
+Note: Microsoft.AspNetCore.Authentication.JwtBearer must be set to version 3.1.10 as the latest versions only support .NET Framework 5!
+
+Note: Tehre are various differences in teh Api: Statup.cs such as the following which were left in place.
+```
+if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+```
+Api Test Client uses enhanced colours, Green for headings, yellow for Input, White for output Grey for Information on program activity, red for errors.
+
---
+++ src/ApiTestClient/Program.cs
@@ -0,0 +1,96 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiTestClient
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Output(ConsoleColor.Green, () => { Console.WriteLine("API test client, initialising..."); });
+
+            Output(ConsoleColor.Yellow, () =>
+            {
+                Console.Write("Please ensure api and identity server are started, and then press any key: ");
+            });
+            Console.ReadKey(true);
+
+            Output(ConsoleColor.Green, () => { Console.WriteLine("\n\nObtain Access Token"); });
+
+            Console.WriteLine("\n\nRequesting Token: ...");
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001/identity");
+            if (disco.IsError)
+            {
+                Output(ConsoleColor.Red, () => { Console.WriteLine(disco.Error); });
+                return;
+            }
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Output(ConsoleColor.Red, () =>
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                });
+                return;
+            }
+
+            Output(ConsoleColor.White, () => { Console.Write($"\n\n[{tokenResponse.AccessToken}]"); });
+            Console.WriteLine("\n\nToken recieved.");
+
+
+            Output(ConsoleColor.Green, () => { Console.WriteLine("\n\nUse Access Token"); });
+
+            Output(ConsoleColor.Yellow, () => { Console.Write("\n\nPress any key to use the token to call the API: "); });
+
+            Console.ReadKey(true);
+
+            Console.WriteLine("\n\nCalling API: ...");
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Output(ConsoleColor.Red, () => { Console.WriteLine(response.StatusCode); });
+
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Output(ConsoleColor.White, () =>
+                {
+                    Output(ConsoleColor.White, () => { Console.WriteLine($"\n\n[{JArray.Parse(content)}]"); });
+                    Console.WriteLine($"\n\nAPI Response recieved.");
+                });
+            }
+
+            Output(ConsoleColor.Yellow, () => { Console.Write("\n\nPress any key to terminate: "); });
+            Console.ReadKey(true);
+            Console.WriteLine("API test client, completed.");
+
+        }
+
+        static void Output(ConsoleColor color, Action act)
+        {
+            var currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            act.Invoke();
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
