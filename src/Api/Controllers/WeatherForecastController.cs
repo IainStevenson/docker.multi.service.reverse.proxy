d2f960bcf5f32263d1243644bd6bd84d3db3d7e6
Iain <iainstevenson@live.co.uk>
2021-01-28 15:48:28 +0000

    Added untested PUT and DELETE methods

--- src/Api/Controllers/WeatherForecastController.cs
+++ src/Api/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-       
+
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly List<WeatherForecast> _forecasts;
         public WeatherForecastController(ILogger<WeatherForecastController> logger, List<WeatherForecast> forecasts)
@@ -35,16 +35,24 @@ namespace Api.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet]
-        [Route("{id:guid}", Name = "GetById", Order =1)]
+        [Route("{id:guid}", Name = "GetById", Order = 1)]
         public async Task<IActionResult> Get(Guid id)
         {
-            return Ok(_forecasts.FirstOrDefault(x=>x.Id == id));
+            return Ok(_forecasts.FirstOrDefault(x => x.Id == id));
         }
 
 
         /// <summary>
-        /// Add one
+        /// Add one. 
         /// </summary>
+        /// <remarks>The client does not need to specify an Id as one will be assigned and reported via the Location header</remarks>
+        /// <example>
+        /// Example body:
+        /// {
+        ///    "temperatureC": 37,
+        ///    "summary": "Balmy"
+        /// }
+        /// </example>
         /// <param name="model">The data as Json body content</param>
         /// <returns></returns>
         [HttpPost]
@@ -56,9 +64,64 @@ namespace Api.Controllers
                 return CreatedAtRoute("GetById", new { id = model.Id }, null);
             }
             else
-            { 
+            {
                 return BadRequest(model);
             }
         }
+
+
+        /// <summary>
+        /// Change one
+        /// </summary>
+        /// <param name="id">The id of the item to update</param>
+        /// <param name="model">The data as Json body content</param>
+        /// <returns>204 NoContent, otherwise 400 BadRequest</returns>
+        [HttpPut]
+        [Route("{id:guid}", Name = "PutById", Order = 2)]
+        public async Task<IActionResult> Put(Guid id, [FromBody] WeatherForecast model)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = _forecasts.FirstOrDefault(x => x.Id == id);
+                if (item != null)
+                {
+                    item.Id = id;
+                    item.Date = model.Date;
+                    item.TemperatureC = model.TemperatureC;
+                    item.Summary = model.Summary;
+                    return NoContent();
+                }
+
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                return BadRequest(model);
+            }
+        }
+        /// <summary>
+        /// Delete one
+        /// </summary>
+        /// <param name="id">The id of the item to delete</param>
+        /// <returns>204 NoContent</returns>
+        [HttpDelete]
+        [Route("{id:guid}", Name = "DeleteById", Order = 3)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (Guid.Empty != id)
+            {
+                var item = _forecasts.FirstOrDefault(x => x.Id == id);
+                if (item != null)
+                {
+                    _forecasts.Remove(item);
+                    return NoContent();
+                }
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
     }
-}
+};
